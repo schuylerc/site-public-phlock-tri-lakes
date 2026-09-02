465c20e173d2b027155a48b4392009ea7140ce63
tntjt555 <nancylucich@gmail.com>
2026-09-02 14:08:27 -0400

    Arrange photo gallery chronologically left-to-right

--- fix_gallery.ps1
+++ fix_gallery.ps1
@@ -1,9 +1,9 @@
 $basePath = 'c:\Users\tntjt\OneDrive\Documents\GitHub\site-public-phlock-tri-lakes\public\pics\gallery'
 $jsPath = 'c:\Users\tntjt\OneDrive\Documents\GitHub\site-public-phlock-tri-lakes\public\js\gallery-data.js'
 
-$co = (Get-ChildItem -Path (Join-Path $basePath 'Community Outreach') -File | Sort-Object LastWriteTime, @{Expression={ if ($_.Name -match '^\d+') { [int64]$matches[0] } else { 0 } }}, Name).Name
-$ph2026 = (Get-ChildItem -Path (Join-Path $basePath '2026 Phlock Photos') -File | Sort-Object LastWriteTime, @{Expression={ if ($_.Name -match '^\d+') { [int64]$matches[0] } else { 0 } }}, Name).Name
-$ph2025 = (Get-ChildItem -Path (Join-Path $basePath '2025 Phlock Photos') -File | Sort-Object LastWriteTime, @{Expression={ if ($_.Name -match '^\d+') { [int64]$matches[0] } else { 0 } }}, Name).Name
+$co = (Get-ChildItem -Path (Join-Path $basePath 'Community Outreach') -File | Sort-Object @{Expression={ if ($_.Name -match '^\d+') { [double]$matches[0] } else { 0 } }}, Name).Name
+$ph2026 = (Get-ChildItem -Path (Join-Path $basePath '2026 Phlock Photos') -File | Sort-Object @{Expression={ if ($_.Name -match '^\d+') { [double]$matches[0] } else { 0 } }}, Name).Name
+$ph2025 = (Get-ChildItem -Path (Join-Path $basePath '2025 Phlock Photos') -File | Sort-Object @{Expression={ if ($_.Name -match '^\d+') { [double]$matches[0] } else { 0 } }}, Name).Name
 
 $content = Get-Content $jsPath -Raw
 
@@ -20,4 +20,4 @@ $arrStr = ($ph2025 | ForEach-Object { "        `"$_`"" }) -join ",`r`n"
 $content = $content -replace '(?s)("2025 Phlock Photos"\s*:\s*\[).*?(\])', "`${1}`r`n$arrStr`r`n    `${2}"
 
 Set-Content -Path $jsPath -Value $content -Encoding UTF8
-Write-Host "Replaced arrays. New 2025 length: $($ph2025.Count)"
+Write-Host "Replaced arrays. New 2026 length: $($ph2026.Count)"
